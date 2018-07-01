d485341b96b78ca4a788d261e3aa54e29ffeeb28
Islomkhuja <nanotexnolagiya@mail.ru>
2018-07-01 18:15:41 +0500

    Registration and Logout completed

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   PORT: 3000,
   MONGO_URL: 'mongodb://localhost/auth',
-  SESSION_SECRET: '68481wdad',
+  SESSION_SECRET: 'R4l7dOZLyZ6dLVWjz3tK21JqmTbNgALP',
   IS_PRODUCTION: false
 };
--- index.js
+++ index.js
@@ -2,37 +2,65 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
 const config = require('./config');
+const mongoose = require('mongoose');
+const session = require('express-session');
+const bcrypt = require('bcrypt-nodejs');
+
+const User = require('./models/User');
+
+const MongoStore = require('connect-mongo')(session);
+
+// database
+mongoose.Promise = global.Promise;
+mongoose.set('debug', config.IS_PRODUCTION);
+mongoose.connection
+  .on('error', error => console.log(error))
+  .on('close', () => console.log('Database connection closed.'))
+  .once('open', () => {
+    const info = mongoose.connections[0];
+    console.log(`Connected to ${info.host}:${info.port}/${info.name}`);
+    // require('./mocks')();
+  });
+mongoose.connect(config.MONGO_URL);
 
 const app = express();
 
-app.set('view engine', 'pug');
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
+// sessions
 app.use(
-  '/js',
-  express.static(path.join(__dirname, 'node_modules', 'jquery', 'dist'))
+  session({
+    secret: config.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+      mongooseConnection: mongoose.connection
+    }),
+    expires: new Date(Date.now() + 60 * 60 * 24 * 30)
+  })
 );
 
-// catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   const err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// error handler
-// eslint-disable-next-line no-unused-vars
-// app.use((error, req, res, next) => {
-//   res.status(error.status || 500);
-//   res.render('error', {
-//     message: error.message,
-//     error: !config.IS_PRODUCTION ? error : {}
-//   });
-// });
+app.set('view engine', 'pug');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res) {
-  res.render('index');
+  if (req.session.userId) {
+    const userId = req.session.userId;
+    const userName = req.session.userName;
+    res.render('index', { userId, userName });
+  } else {
+    res.redirect('/login');
+  }
+});
+
+app.get('/logout', function(req, res) {
+  if (req.session) {
+    // delete session object
+    req.session.destroy(() => {
+      res.redirect('/');
+    });
+  } else {
+    res.redirect('/');
+  }
 });
 
 app.get('/login', function(req, res) {
@@ -40,7 +68,7 @@ app.get('/login', function(req, res) {
 });
 
 app.post('/login', function(req, res) {
-  let errors = {};
+  var errors = {};
   res.render('login', errors);
 });
 
@@ -50,7 +78,58 @@ app.get('/register', function(req, res) {
 
 app.post('/register', function(req, res) {
   let errors = {};
-  res.render('register', errors);
+  const name = req.body.name;
+  const email = req.body.email;
+  const password = req.body.password;
+  const terms = req.body.terms;
+  if (!terms) {
+    errors.formError = 'Terms no checked';
+  } else if (!name || !email || !password) {
+    if (!name) errors.errorName = 'Name not found';
+    if (!email) errors.errorEmail = 'Email not found';
+    if (!password) errors.errorPassword = 'Password not found';
+  } else if (name.length < 3) {
+    errors.errorName = 'Length Name min 3 symbol';
+  } else if (
+    !/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+      email
+    )
+  ) {
+    errors.errorEmail = 'Email in valid';
+  } else if (password.length < 6) {
+    errors.errorPassword = 'Length Name min 6 symbol';
+  } else {
+    errors.noError = true;
+  }
+
+  if (errors.noError) {
+    User.findOne({
+      email
+    }).then(user => {
+      if (!user) {
+        bcrypt.hash(password, null, null, (err, hash) => {
+          User.create({
+            name,
+            email,
+            password: hash
+          })
+            .then(user => {
+              req.session.userId = user.id;
+              req.session.userName = user.name;
+              res.redirect('/');
+            })
+            .catch(err => {
+              errors.formError = 'Error';
+            });
+        });
+      } else {
+        errors.errorEmail = 'E-mail isset';
+        res.render('register', errors);
+      }
+    });
+  } else {
+    res.render('register', errors);
+  }
 });
 
 app.get('/forgot', function(req, res) {
